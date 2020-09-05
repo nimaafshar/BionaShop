a17f7134d2dce76ab6086bd477e69bc0ecac1564
Nima <nimaafshar79@gmail.com>
2020-09-05 23:15:35 +0430

    templates integration

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,5 @@ app_name = 'shop'
 urlpatterns = [
     path('detail/<int:product_id>', views.detail, name='detail'),
     path('bill', views.show_bill, name='bill'),
-    path('order/<str:bill_id>', views.order_status, name='order_status')
+    path('order', views.order_status, name='order_status')
 ]
